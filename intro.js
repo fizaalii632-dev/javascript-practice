a789076381fb92002b79b6ba085509857dedc4c2
Zeeshan632 <zeeshanadil632@gmail.com>
2026-05-14 22:26:49 +0500

    Added HTML file

--- intro.js
+++ intro.js
@@ -1,3 +1,5 @@
-console.log('Hello World')
-console.log('Hello World')
-console.log('Hello World')
+//
+let mystr = `Total cost: $${45 + 50} /n Thanks for coming`
+console.log(mystr)
+let popup = window.alert ( ' hello world ' ) 
+console.log(popup)
